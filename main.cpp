c0e9693d663e93ad4be5702f0dc17e538b1739e2
krisckk <krischou@gapp.nthu.edu.tw>
2024-12-15 19:25:23 +0800

    First

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+vector<string> gameList;
+
+int main() {
+	gameList.push_back("far cry 5");
+    gameList.push_back("God of war(2018)");
+    gameList.push_back("The Witcher 3");
+    gameList.push_back("The Last of Us");
+    gameList.push_back("Hogwarts Legacy");
+    gameList.push_back("Lies of P");
+    gameList.push_back("Marvel's Gaurdians of the Galaxy");
+    gameList.push_back("Red Dead Redemption 2");
+	return 0;
+}
