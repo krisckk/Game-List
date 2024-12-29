49c0b582a01df1666b2af32c7193e33b91c8ed54
周伯穎 <144316141+krisckk@users.noreply.github.com>
2024-12-30 01:07:18 +0800

    12/30

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 vector<string> gameList;
 
 int main() {
-	gameList.push_back("far cry 5");
+	gameList.push_back("Far Cry 5");
     gameList.push_back("God of war(2018)");
     gameList.push_back("The Witcher 3");
     gameList.push_back("The Last of Us");
@@ -13,5 +13,6 @@ int main() {
     gameList.push_back("Lies of P");
     gameList.push_back("Marvel's Gaurdians of the Galaxy");
     gameList.push_back("Red Dead Redemption 2");
+    gameList.push_back("Assassin's Creed IV: Black Flag");
 	return 0;
 }
