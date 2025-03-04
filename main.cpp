87cbeebbac9a1d6962894270a304f05e56fb7121
周伯穎 <144316141+krisckk@users.noreply.github.com>
2025-03-04 18:13:57 +0800

    Indiana Jones and The Great Circle

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 vector<string> gameList;
 
 int main() {
+    // 2024
 	gameList.push_back("Far Cry 5");
     gameList.push_back("God of war(2018)");
     gameList.push_back("The Witcher 3");
@@ -14,5 +15,16 @@ int main() {
     gameList.push_back("Marvel's Gaurdians of the Galaxy");
     gameList.push_back("Red Dead Redemption 2");
     gameList.push_back("Assassin's Creed IV: Black Flag");
+    // 2025
+    gameList.push_back("Middle Earth: Shadow of War");
+    gameList.push_back("Assassin's Creed: Origins");
+    gameList.push_back("Nine sols");
+    gameList.push_back("Cyberpunk 2077");
+    gameList.push_back("Cyberpunk 2077: Phatom Library");
+    gameList.push_back("Deus Ex: Mankind Divided");
+    gameList.push_back("The outer worlds");
+    gameList.push_back("Borderlands 3");
+    gameList.push_back("Avowed");
+    gameList.push_back("Indiana Jones and the Great Circle");
 	return 0;
 }
